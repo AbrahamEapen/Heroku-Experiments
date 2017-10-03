dabf7d0fc03a97de9f303d101ce7225e2bcd5182
Abe Eapen <abe.eapen90@gmail.com>
2017-10-03 17:39:21 -0400

    Heroku Experiment

---
+++ twitter.py
@@ -0,0 +1,31 @@
+#Dependencies
+import tweepy
+import time
+import json
+import os
+
+# Twitter API Keys
+consumer_key = os.environ.get('consumer_key')
+consumer_secret = os.environ.get('consumer_secret')
+access_token = os.environ.get('access_token')
+access_token_secret = os.environ.get('access_token_secret')
+
+# Setup Tweepy API Authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())\
+
+# api.update_status("Heroku Test")
+
+counter = 0
+
+def deployment(counter): 
+    api.update_status("Heroku Test"+ str(counter))
+    print("Heroku Test"+ str(counter))
+
+# deployment(counter)
+
+while(True):
+    deployment(counter)
+    counter += 1
+    time.sleep(5)
